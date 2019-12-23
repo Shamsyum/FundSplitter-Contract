b1ee0880d52042e6c9694cbd6ea28345ed4e860e
Shams Coding Blockchain <55982732+Shamsyum@users.noreply.github.com>
2019-12-23 18:44:50 +0500

    Added the functionality, So the fund owner claim and get their funds by their self.

--- FundSplitter.sol
+++ FundSplitter.sol
@@ -1,32 +1,50 @@
 pragma solidity 0.5.11;
 
-    // Splitting mony from one account into two accuounts equall.
+    // Splitting mony from one account into two accounts equally.
 
 contract Splitter{ 
     
     mapping(address => uint)balance;
-    
+    address[] addresses;
+    modifier canWithdraw(){
+        for(uint i=0; i<addresses.length; i++){
+            if(addresses[i] == msg.sender && balance[address(msg.sender)] > 0){
+                _;
+            }
+            
+        }
+    }
     function splitFund(address payable  _address1, address payable _address2)public payable{ 
         
         uint remainder = msg.value % 2;
         uint half = msg.value/2;
+        
         if(remainder == 1 wei){
             
         half = (msg.value - 1) / 2;
         
+        //If the amount passed odd in wei
         //Send 1 wei back to the invocker
         msg.sender.transfer(1 wei); 
         
-        //Half of the total amount passed 
-        _address1.transfer(half);
-        _address2.transfer(half);
-        
+        balance[_address1] = half;
+        balance[_address2] = half;
+        addresses.push(_address1);
+        addresses.push(_address2);
         }else{
             
-        //Half of the total amount passed
-        _address1.transfer(half);
-        _address2.transfer(half); 
-       
+        
+        balance[_address1] = half;
+        balance[_address2] = half; 
+        addresses.push(_address1);
+        addresses.push(_address2);
         }
+    }
+    
+    //By this function, the fund owners will get their funds, associated with their address 
+    function withdrawFund()public payable canWithdraw{
+                msg.sender.transfer(balance[address(msg.sender)]);
+                balance[address(msg.sender)] = 0;
+                
     }
 }
