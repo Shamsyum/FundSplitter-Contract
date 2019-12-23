eae636cd8ad452905688f39f15470a6ac8628dc5
Shams Coding Blockchain <55982732+Shamsyum@users.noreply.github.com>
2019-12-23 11:22:01 +0500

    Improved Functionality

--- FundSplitter.sol
+++ FundSplitter.sol
@@ -1,14 +1,32 @@
-pragma solidity ^0.5.0;
+pragma solidity 0.5.11;
 
     // Splitting mony from one account into two accuounts equall.
 
 contract Splitter{ 
     
-    function splitFund(address payable  _address1, address payable _address2)public payable{ 
-        require(msg.value % 2 == 0 wei);
-        _address1.transfer(msg.value/2); //Half of the total amount passed 
-        _address2.transfer(msg.value/2); //Half of the total amount passed
-        
-    }
+    mapping(address => uint)balance;
     
+    function splitFund(address payable  _address1, address payable _address2)public payable{ 
+        
+        uint remainder = msg.value % 2;
+        uint half = msg.value/2;
+        if(remainder == 1 wei){
+            
+        half = (msg.value - 1) / 2;
+        
+        //Send 1 wei back to the invocker
+        msg.sender.transfer(1 wei); 
+        
+        //Half of the total amount passed 
+        _address1.transfer(half);
+        _address2.transfer(half);
+        
+        }else{
+            
+        //Half of the total amount passed
+        _address1.transfer(half);
+        _address2.transfer(half); 
+       
+        }
+    }
 }
