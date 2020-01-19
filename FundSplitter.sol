9a133e18cd85ac56da73d1f038c7c4fd57fcd6b2
Shams Coding Blockchain <55982732+Shamsyum@users.noreply.github.com>
2020-01-19 10:52:04 +0500

    Some logic and style improved in FundSplitter.sol

--- FundSplitter.sol
+++ FundSplitter.sol
@@ -5,14 +5,15 @@ pragma solidity 0.5.11;
 contract Splitter{
     
     mapping(address => uint) public balances;
-    event FundSplitted(address, uint, address, address);
-    event WithdrawDone(address, uint);
+    event FundSplitted(address sender, uint value , address address1, address address2);
+    event WithdrawDone(address sender, uint amount);
      
     function splitFund(address payable  _address1, address payable _address2)public payable{ 
+        emit FundSplitted(msg.sender,msg.value, _address1, _address2);
         uint remainder = msg.value % 2;
         uint half = msg.value/2;
         balances[_address1] += half;
-        balances[_address2] += half;emit FundSplitted(msg.sender,msg.value, _address1, _address2);
+        balances[_address2] += half;
         if(remainder > 0){
             msg.sender.transfer(1 wei);
         }
@@ -20,10 +21,10 @@ contract Splitter{
     
     //By this function, every Fund owner can withdraw their funds by theirself
     function withdrawFund(uint _amount)public returns(bool) {
-        require(balances[msg.sender] >= _amount);
+        require(balances[msg.sender] >= _amount, 'Insufficient balance');
         balances[msg.sender] -= _amount;
-        msg.sender.transfer(_amount); emit WithdrawDone(msg.sender, _amount);
-        
+        emit WithdrawDone(msg.sender, _amount);
+        msg.sender.transfer(_amount);
         return true;
     }
 }
